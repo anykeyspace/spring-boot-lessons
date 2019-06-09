25de3f0677324073c48caa9b3c9fffecbe64e31c
Anykey Space <20nomad12@mail.ru>
2019-06-09 18:38:54 +0300

    Building REST services with Spring lesson. Before Simplifying Link Creation

---
+++ src/main/java/com/anykeyspace/springbootlessons/SpringBootLessonsApplication.java
@@ -0,0 +1,13 @@
+package com.anykeyspace.springbootlessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLessonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLessonsApplication.class, args);
+	}
+
+}
